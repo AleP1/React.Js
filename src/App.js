253249df617b6801c08b0b8a5d81aba879f99d24
AleP1 <63692378+AleP1@users.noreply.github.com>
2021-06-18 14:27:41 -0300

    clase 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+
 import './App.css';
+import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
       
+      <header className="App-header">
+      <div>
+        <h1>Bik0</h1>
+        <p>Alejo PIñeiro</p>
+      </div>
+        <NavBar />
       </header>
     </div>
   );

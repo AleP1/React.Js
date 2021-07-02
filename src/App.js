ea5ba6d5c2aca524ec49872a98d4a766a7d517e2
AleP1 <63692378+AleP1@users.noreply.github.com>
2021-07-01 23:59:44 -0300

    clase 8

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { NavBar } from './components/NavBar/NavBar';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
-import { ItemDetailContainer} from './components/ItemDetailContainer/ItemDetailContainer';
+import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 
-function App() {
-
-
+export function Ap() {
   return (
-
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-
-        <div>
-          <h1>Bik0</h1>
-        </div>
-        <ItemListContainer/>
-        <ItemDetailContainer/>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/">
+            <ItemListContainer />
+          </Route>
+          <Route path="/category/:Id">
+            <ItemListContainer />
+          </Route>
+          <Route path="/Item/:Id">
+            <ItemDetailContainer />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
-export default App;
+//function App() {
+  //return (
+
+    //<div className="App">
+     // <header className="App-header">
+      //  <NavBar />
+
+      //  <div>
+      //    <h1>Bik0</h1>
+      //  </div>
+      //  <ItemListContainer/>
+      //  <ItemDetailContainer />
+     // </header>
+    //</div>
+ // );
+//}
+
+//export default App;
--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,15 +1,27 @@
 import "./Item.css"
 import {ItemCount} from "../itemCount/ItemCount"
+import {Link, NavLink} from 'react-router-dom'
+
 export const Item = ({item, onAdd})=>{
     console.log(item)
     return (
-        <div className="card">
+        <Link to='item/:id'>
+        <article className="card">
+            <NavLink to='/category/:Id'>
             <img className="card__img"src={item.pictureUrl} alt="fotito"/>
+            </NavLink>
+            <NavLink to='/category/:Id'>
             <p className="card__title">{item.title}</p>
+            </NavLink>
+            <NavLink to='/category/:Id'>
             <p className="card__price">{item.price}</p>
+            </NavLink>
+            <NavLink to='/category/:Id'>
             <p className="card__des">{item.description}</p>
+            </NavLink>
             <ItemCount stock={5} inicial={0}/>
-        </div>
+        </article>
+        </Link>
     )
 
 
